f0e0c98c4b49025a0255585350c2c7f53c81a188
doleaga2 <oleaga.diana@gmail.com>
2022-12-18 11:32:49 -0500

    adding button to places

--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,14 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+              {/* dont do views/places/index.jsx because you're just trying to have browser go to /places its not how it is for html links  */}
+              <a href="/places"> 
+    <button className="btn-primary">Places Page</button>
+  </a>
           </main>
       </Def>
     )
   }
-  
 
 module.exports = home
