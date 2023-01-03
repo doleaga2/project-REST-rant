a4758eeeac79cb3d5f8cf20236cbc074b879d53a
doleaga2 <oleaga.diana@gmail.com>
2023-01-02 22:53:48 -0500

    change h1 tag

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-Rant</h1>
               <div>
                 <img src="/images/penne-pasta.jpg" alt="Penne Pasta" />
                 <div>
