0c9a36998d1438d40fd987a1ca1e33ad12ea69a0
doleaga2 <oleaga.diana@gmail.com>
2023-01-02 22:06:13 -0500

    Adding Image to the home view

--- views/home.jsx
+++ views/home.jsx
@@ -6,7 +6,13 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
-              <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+              <div>
+                <img src="/images/penne-pasta.jpg" alt="Penne Pasta" />
+                <div>
+                  Photo By <a href="https://unsplash.com/@bleiplays33">Ben Lei</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+              </div>
+
               {/* dont do views/places/index.jsx because you're just trying to have browser go to /places its not how it is for html links  */}
               <a href="/places"> 
     <button className="btn-primary">Places Page</button>
