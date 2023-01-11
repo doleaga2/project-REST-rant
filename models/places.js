f388846fbd3f5aeda2f1105c083526f8de4344b9
doleaga2 <oleaga.diana@gmail.com>
2023-01-10 19:15:19 -0500

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Semicolon Cafe',
+    city: 'New York',
+    state: 'NY',
+    cuisines: 'cafe, sandwiches',
+    pic: '/images/semicoloncafe.jpg'
+  }, {
+    name: 'ViVi Bubble Tea',
+    city: 'New York',
+    state: 'NY',
+    cuisines: 'bubble tea',
+    pic: '/images/vivi-boba.jpg'
+  }]
