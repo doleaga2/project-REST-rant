d07f6e6466012cd1f25451ec3ddeae5304572151
doleaga2 <oleaga.diana@gmail.com>
2023-01-12 19:12:57 -0500

    problems with mongoose

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// where we connect to mongoose
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+module.exports.Place = require('./places')
+// making access point ot all other models
--- models/places.js
+++ models/places.js
@@ -1,13 +1,27 @@
-module.exports = [{
-    name: 'Semicolon Cafe',
-    city: 'New York',
-    state: 'NY',
-    cuisines: 'coffee, sandwiches',
-    pic: '/images/semicoloncafe.jpg'
-  }, {
-    name: 'ViVi Bubble Tea',
-    city: 'New York',
-    state: 'NY',
-    cuisines: 'bubble tea',
-    pic: '/images/vivi-boba.jpg'
-  }]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+// module.exports = [{
+//     name: 'Semicolon Cafe',
+//     city: 'New York',
+//     state: 'NY',
+//     cuisines: 'coffee, sandwiches',
+//     pic: '/images/semicoloncafe.jpg'
+//   }, {
+//     name: 'ViVi Bubble Tea',
+//     city: 'New York',
+//     state: 'NY',
+//     cuisines: 'bubble tea',
+//     pic: '/images/vivi-boba.jpg'
+//   }]
